b5f67a88a9b3de42f0062ddd47407440d3896e02
highsung <highsung@qq.com>
2017-11-03 08:43:21 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import time
+from browser import *
+from selenium.common.exceptions import *
+from mongodb1 import *
+
+b = get_chrome_browser()
+
+
+def get_yhq_yxq(url):
+    yxq = []
+    try:
+        b.get(url)
+        ele = b.find_element_by_xpath('//*[@id="mx_6"]/div/div/span[3]')
+        print ele.text
+        yxq = ele.text.split('-')
+    except NoSuchElementException:
+        print 'no yhq'
+        pass
+    return yxq
+
+
+def update(g):
+    try:
+        url = g['shortLinkUrl']
+        print url
+        if len(url.strip()) == 0:
+            print 'url is empty'
+            delete_goods(g)
+        else:
+            yxq = get_yhq_yxq(url)
+            if len(yxq):
+                g['couponEffectiveStartTime'] = yxq[0]
+                g['couponEffectiveEndTime'] = yxq[1]
+                if compare_today(yxq[1]):
+                    print 'yhq is guo qi'
+                    delete_goods(g)
+                else:
+                    update_goods(g)
+            else:
+                print 'no yxq'
+                delete_goods(g)
+    except KeyError as e:
+        print 'json no key:' + str(e)
+        delete_goods(g)
+
+
+def main():
+
+    while True:
+        goods = get_goods()
+
+        print 'update count:'
+        print goods.count()
+
+        # no data to update
+        if goods.count() == 0:
+            time.sleep(5)
+            continue
+
+        for g in goods:
+            # print g
+            update(g)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ mongodb1.py
@@ -0,0 +1,66 @@
+#coding=utf-8
+
+from datetime import datetime
+import pymongo
+from pymongo import MongoClient
+
+# 连接mongodb数据库
+client = MongoClient('mongodb://mongodb:27017/')
+# 指定数据库名称
+db = client.tbk_database
+# 获取非系统的集合
+db.collection_names(include_system_collections=False)
+print (db.collection_names())
+
+
+def compare_today(day):
+    today = datetime.today()
+    today = datetime(today.year, today.month, today.day, 0, 0, 0)
+    ymd = day.split('.')
+    day1 = datetime(ymd[0], ymd[1], ymd[2])
+    if today > day1:
+        return 1
+    else:
+        return 0
+
+
+def get_today_start():
+    today = datetime.today()
+    today_start = datetime(today.year, today.month, today.day, 1, 0, 0)
+    return today_start
+
+
+def get_goods():
+    c = db.goods
+    d = get_today_start()
+    # return c.find({'updateAt': {"$lt": d}, 'isDelete': False}).batch_size(30).sort("updateAt",1).limit(100)
+    return c.find({'updateAt': {"$lt": d}}).batch_size(30).sort("updateAt",1).limit(100)
+
+
+def update_goods(g):
+    print 'update'
+    c = db.goods
+    g['updateAt'] = datetime.today()
+    c.update({'_id': g['_id']}, g)
+
+
+def delete_goods(g):
+    print 'delete'
+    c = db.goods
+    g['updateAt'] = datetime.today()
+    g['isDelete'] = True
+    c.update({'_id': g['_id']}, g)
+
+
+# today = datetime.today()
+# today_start1 = datetime(today.year, today.month, today.day, 1, 0, 1)
+# today_start2 = datetime(today.year, today.month, today.day, 1, 0, 1)
+# print today_start1
+# print today_start2
+# if today_start1 > today_start2:
+#     print '1>2'
+# else:
+#     print '1<2'
+
+# today = datetime.today()
+# print today
